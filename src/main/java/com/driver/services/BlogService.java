67d9aa91893d923d4c50ecaba583b2d50d0e06af
shibanisaha <shibanis138@gmail.com>
2023-02-18 19:49:37 +0530

    Commit9

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -25,8 +25,9 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-        User user = userRepository1.findById(userId).get();
         Blog blog = new Blog();
+        if(userRepository1.findById(userId).isPresent()){
+            User user = userRepository1.findById(userId).get();
             blog.setTitle(title);
             blog.setContent(content);
 
@@ -34,6 +35,7 @@ public class BlogService {
             List<Blog> blogArrayList = user.getBlogList();
             blogArrayList.add(blog);
             userRepository1.save(user);
+        }
 
         return blog;
     }
